91c2ac2ae249b6c6daeaa8153c45278001dde527
alanakil <aakil@math.uh.edu>
2023-04-22 01:00:07 -0500

    concise import

--- docs/conf.py
+++ docs/conf.py
@@ -14,7 +14,7 @@ with open(os.path.join(PACKAGE_DIR, "VERSION")) as f:
 sys.path.append(ROOT_DIR)
 sys.path.append(PACKAGE_DIR)
 
-from src import plastic_balanced_network
+import plastic_balanced_network
 
 # -- Project information -----------------------------------------------------
 
